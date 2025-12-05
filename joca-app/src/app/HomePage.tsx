642fbc504f65646c799b803658180e8da06a51ab
henrylee746 <henry.lee746@gmail.com>
2025-12-04 22:44:44 -0500

    Added interactive hover buttons

--- joca-app/src/app/HomePage.tsx
+++ joca-app/src/app/HomePage.tsx
@@ -14,6 +14,8 @@ import { Calendar, FolderTree, Users, Vote } from "lucide-react";
 import Link from "next/link";
 import React from "react";
 import { HeroParallax } from "@/components/ui/hero-parallax";
+import { InteractiveHoverButton } from "@/components/ui/interactive-hover-button";
+import { FlippingCard } from "@/components/ui/flipping-card";
 
 export const products = Array.from({ length: 15 }, (_, index) => ({
   title: `Joca${index + 1}`,
@@ -21,5 +23,47 @@ export const products = Array.from({ length: 15 }, (_, index) => ({
 }));
 
 export function HomePage() {
-  return <HeroParallax products={products} />;
+  return (
+    <>
+      <HeroParallax products={products} />
+      <nav className="flex gap-8 items-center justify-center">
+        <Link href="signup">
+          <InteractiveHoverButton>Become a Member</InteractiveHoverButton>
+        </Link>
+        <Link href="events">
+          <InteractiveHoverButton>View Events</InteractiveHoverButton>
+        </Link>
+      </nav>
+      <section className="w-full flex gap-8 items-center justify-center p-6 mb-16">
+        <Card className="w-xs">
+          <CardHeader>
+            <Calendar className="mb-2" />
+            <CardTitle>Cultural Events</CardTitle>
+            <CardDescription>
+              Join us for festivals, workshops and gatherings celbrating
+              Jamaican culture
+            </CardDescription>
+          </CardHeader>
+        </Card>
+        <Card className="w-xs">
+          <CardHeader>
+            <Users className="mb-2" />
+            <CardTitle>Community</CardTitle>
+            <CardDescription>
+              Connect with fellow members and building lasting friendships
+            </CardDescription>
+          </CardHeader>
+        </Card>
+        <Card className="w-xs">
+          <CardHeader>
+            <Vote className="mb-2" />
+            <CardTitle>Member Benefits</CardTitle>
+            <CardDescription>
+              Voting rights, exclusing events, and more for active members
+            </CardDescription>
+          </CardHeader>
+        </Card>
+      </section>
+    </>
+  );
 }
