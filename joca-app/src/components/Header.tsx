f186b2a3812ff6d35bc77b32afae6d15d4812120
henrylee746 <henry.lee746@gmail.com>
2025-12-06 18:51:42 -0500

    Reapplied dark icon sticking to right of header

--- joca-app/src/components/Header.tsx
+++ joca-app/src/components/Header.tsx
@@ -4,7 +4,7 @@ import { AnimatedThemeToggler } from "@/components/ui/animated-theme-toggler";
 
 const Header = () => {
   return (
-    <header className="sticky flex flex-col gap-6  sm:flex-row  items-center p-4 px-8 top-0 z-50 w-full border-b border-border bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+    <header className="sticky flex flex-col gap-6  sm:flex-row  items-center justify-center sm:justify-between p-4 px-8 top-0 z-50 w-full border-b border-border bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="container flex  h-24 items-center justify-center sm:justify-between">
         <div className="flex flex-col sm:flex-row items-center gap-8">
           <Link href="/" className="flex items-center gap-2">
